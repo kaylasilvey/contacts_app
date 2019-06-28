64ea682cee57529ed236882676e7b51bb19708b8
Kayla Silvey <ksilvey19@gmail.com>
2019-06-27 20:53:57 -0500

    implement RESTful index/show/create/update/delete actions

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+contacts = Contact.create([
+  {
+    first_name: "Bob",
+    last_name: "Smith",
+    email: "bob.smith@email.com",
+    phone_number: "123-456-7890",
+  },
+  {
+    first_name: "Mary",
+    last_name: "Jones",
+    email: "mary.jones@email.com",
+    phone_number: "234-567-8901",
+  },
+  {
+    first_name: "Ann",
+    last_name: "Davidson",
+    email: "ann.davidson@email.com",
+    phone_number: "345-678-9012",
+  },
+])
